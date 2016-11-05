b47d1636d7fa5f8db1647528f349b40b2f11ae17
AlvinTech <alvalp@live.cn>
2016-11-05 21:48:03 +0800

    add  adapter pattern demo text

--- src/main/java/Adapter/Client.java
+++ src/main/java/Adapter/Client.java
@@ -3,6 +3,13 @@ package Adapter;
 /**
  * Created by Alvin on 16/11/5.
  */
+
+/*
+系统中原有已经实现的ICircle类。
+现在由于新功能要求，要求所有的图形类需要继承 Shape 接口。
+因此原先实现的ICircle就不满足现在的要求。而且接口的返回值有严格要求。
+
+ */
 public class Client {
     public static void main(String[] args) {
         ICircle iCircle = new ICircle(1.5);
